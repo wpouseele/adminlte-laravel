fd9f7761a17aba2c55fa77b4d4c7e1f70b2bbf12
Wim Pouseele <wim.pouseele@gmail.com>
2016-01-19 21:07:23 +0100

    Initial commit

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        <a href="https://github.com/wpouseele/vcloudone-mgmt"></a><b>vcloudone-mgmt</b></a>. Frontend for the vcloudone-mgmt application.
+        <a href="https://github.com/wpouseele/vcloudone-mgmt"></a><b>vcloudone-mgmt</b></a>. Management application for all vCloudOne related tasks.
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2015 <a href="http://conxion.be">ConXioN</a>.</strong> Created by <a href="http://github.com/wpouseele">Wim Pouseele</a>. See code at <a href="https://github.com/wpouseele/adminlte-laravel">Github</a>
