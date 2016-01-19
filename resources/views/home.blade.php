7c062f378844ab3429b4fb920bac77b926f8b9de
Wim Pouseele <wim.pouseele@gmail.com>
2016-01-19 17:28:34 +0100

    Initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,10 +10,15 @@
 		<div class="row">
 			<div class="col-md-10 col-md-offset-1">
 				<div class="panel panel-default">
-					<div class="panel-heading">Home</div>
+					<div class="panel-heading">Welcome</div>
 
 					<div class="panel-body">
-						You are logged in!
+						This app is used to manage all aspects of a vCloudOne project.<br><br>
+						<ul>
+							<li><a href="/project/create">Create a new project</a></li>
+							<li><a href="/company/">View all companies</a></li>
+							<li><a href="/product/">View all products</a></li>
+						</ul>
 					</div>
 				</div>
 			</div>
