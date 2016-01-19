672b03899c929c70082133478ab07259036041bf
Wim Pouseele <wim.pouseele@gmail.com>
2016-01-19 21:19:02 +0100

    Initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@ desired effect
 |               | sidebar-mini                            |
 |---------------------------------------------------------|
 -->
-<body class="skin-blue sidebar-mini">
+<body class="skin-green sidebar-mini">
 <div class="wrapper">
 
     @include('layouts.partials.mainheader')
