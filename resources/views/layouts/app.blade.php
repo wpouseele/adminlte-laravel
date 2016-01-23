739aa73ad812ccafda916477ff29f00ae976de6b
Wim Pouseele <wim.pouseele@gmail.com>
2016-01-23 13:28:26 +0100

    Move additional html headers to own view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@ This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
-
-@include('layouts.partials.htmlheader')
+@section('htmlheader')
+    @include('layouts.partials.htmlheader')
+@show
 
 <!--
 BODY TAG OPTIONS:
