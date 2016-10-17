fc22033d02e7c515b3dd9a1ce5f08ff6cc998266
Wim Pouseele <wim.pouseele@gmail.com>
2016-10-17 21:09:19 +0200

    Remove git versioning

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        <a href="https://github.com/wpouseele/vcloudone-mgmt"></a><b>vcloudone-mgmt</b></a>. {{ \Tremby\LaravelGitVersion\GitVersionHelper::getVersion() }}. {{ trans('adminlte_lang::message.descriptionpackage') }}
+        <a href="https://github.com/wpouseele/vcloudone-mgmt"></a><b>vcloudone-mgmt</b></a>. {{ trans('adminlte_lang::message.descriptionpackage') }}
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2015 <a href="http://conxion.be">ConXioN</a>.</strong> {{ trans('adminlte_lang::message.createdby') }} <a href="http://github.com/wpouseele">Wim Pouseele</a>. {{ trans('adminlte_lang::message.seecode') }} <a href="https://github.com/wpouseele/adminlte-laravel">Github</a>
